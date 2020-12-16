68ad85d2eb54d2ef9321c7a6d71ca30a9f7dbdc8
Dmitry Ponyatov <dponyatov@gmail.com>
2020-12-17 02:32:44 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/cloj/core.clj
+++ src/cloj/core.clj
@@ -6,6 +6,11 @@
 (defn hello [] (println Hello))
 ; (hello)
 
+(nil? `(false))
+
+(spit "apt.txt" "git make wget\n")
+(spit "apt.txt" "leiningen clojure\n" :append true)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
